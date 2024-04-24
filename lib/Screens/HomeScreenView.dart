a4586a9f048d658c97da7070f170f4e8ccdcaef9
Thanon_UP <thanon.sspk@gmail.com>
2024-04-24 23:27:58 +0700

    feat : add header menu app bar and button.

--- lib/Screens/HomeScreenView.dart
+++ lib/Screens/HomeScreenView.dart
@@ -11,6 +11,38 @@ class _HomeScreenViewState extends State<HomeScreenView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.asset(
+          "assets/image/netflix-logo.png",
+          width: 120,
+          height: 50,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: InkWell(
+              onTap: () {},
+              child: const Icon(
+                Icons.search,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: Colors.blue,
+              width: 25,
+              height: 25,
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          )
+        ],
+      ),
       body: Center(
         child: Text("Hi This is a Home"),
       ),
